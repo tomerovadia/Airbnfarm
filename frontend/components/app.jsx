8a1ccb0dfe4c5982bf0135def04104316d798822
Tomer Ovadia <tomertomertomer@gmail.com>
2017-04-18 17:10:39 -0400

    Deploy to heroku for first time

---
+++ frontend/components/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+
+  return (
+    <h1>WELCOME TO THE ROOT OF AIRNBN!</h1>
+  );
+
+};
--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import {Router, Route, IndexRoute, hashHistory} from 'react-router';
+import { Provider } from 'react-redux';
+import App from './app';
 
-export default () => {
-
+export default (props) => {
 
   return (
-    <h1>WELCOME TO AIRNBN!</h1>
+    <h1>WELCOME</h1>
+    // <Provider store={props.store}>
+    //   <Router history={ hashHistory }>
+    //     <Route path='/' component={App} />
+    //   </Router>
+    // </Provider>
   );
 
 };
