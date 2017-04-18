649dc5c53fb739ffb51d9aa1688be334e59fc347
Tomer Ovadia <tomertomertomer@gmail.com>
2017-04-18 15:31:22 -0400

    Downcase rootReducer and sessionReducer, add store to entry file

--- frontend/airnbn.jsx
+++ frontend/airnbn.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {login, logout, signup} from './util/session_api_util';
+import configureStore from './store/store.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   window.login = login;
   window.logout = logout;
   window.signup = signup;
+
+  const store = configureStore();
 
   const root = document.getElementById('root');
   ReactDOM.render(
