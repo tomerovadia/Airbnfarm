a0d3fc5e29cb3c1ee19dcbefcf5dcf6ea8694618
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-04-18 12:09:20 -0400

    Complete Rails auth, added BCrypt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user, :logged_in?
+
+  def login!(user)
+    @current_user = user
+    session[:session_token] = user.reset_session_token
+  end
+
+  def logout!
+    current_user.reset_session_token if current_user
+    session[:session_token] = nil
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def current_user
+    return nil if session[:session_token].nil?
+    @current_user = User.find_by(session_token: session[:session_token])
+    @current_user
+  end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,39 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true
+  validates :password, length: {minimum: 6, allow_nil: true}
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+
+  def self.generate_session_token
+    RandomSecure.urlsafe_base64(16)
+  end
+
+  def reset_session_token
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return user if user && user.is_password?(password)
+    nil
+  end
+
+  private
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
+
 end
