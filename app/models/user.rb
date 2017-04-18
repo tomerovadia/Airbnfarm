e401756ee6d8c243ab808e37ab6c3d2fe26b2788
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-04-18 12:11:52 -0400

    Add UsersController, added gems for dev, annotated models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string           not null
+#  password_digest :string
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :password, length: {minimum: 6, allow_nil: true}
