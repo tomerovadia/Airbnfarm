9a4a18c853638b50649d8916314fe6ade083299d
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-04-18 11:50:54 -0400

    Set up route for static_pages, set up entry file

---
+++ frontend/airnbn.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(
+    <h1>Welcome to Airnbn</h1>,
+    root
+  );
+});
