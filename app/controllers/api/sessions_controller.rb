d1aae74847aa7b6ac2cfe1e1f694ec503e367922
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-04-18 12:31:47 -0400

    Work on SessionsController and UsersController, create jbuilder for users/show

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,7 +1,17 @@
 class Api::SessionsController < ApplicationController
 
   def create
+    @user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
 
+    if @user
+      login!(@user)
+      redirect_to user_url(@user)
+    else
+      
+    end
   end
 
   def destroy
