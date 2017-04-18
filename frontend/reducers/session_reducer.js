08283a5b84e539537e632ec6ba3f84c7ff4623b2
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-04-18 15:09:53 -0400

    Complete session_actions, store and session_api_util, create session_reducer and root_reducer

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,11 @@
+import { login, logout, signup } from '../actions/session_actions';
+import {RECEIVE_CURRENT_USER, RECEIVE_ERRORS} from '../'
+
+const _initialState = {
+  currentUser: null
+};
+
+export default (oldState = _initialState, action) => {
+
+
+};
--- frontend/util/session_api_util.js
+++ frontend/util/session_api_util.js
@@ -1,6 +1,3 @@
-
-
-
 export const login = (user) => {
   return $.ajax({
     method: 'post',
