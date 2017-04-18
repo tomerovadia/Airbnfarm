7772f4395a0284662a721a4a2cd5ba087be40145
Tomer Ovadia <tomertomertomer@gmail.com>
2017-04-18 15:09:53 -0400

    Complete session_actions, store and session_api_util, create session_reducer and root_reducer

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,48 @@
+import * as SessionAPIUtil from '../util/session_api_util';
+
+// Constants
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_USER';
+export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+
+
+// Regular object action creators
+
+export const receiveCurrentUser = (user) => {
+  return {
+    type: RECEIVE_CURRENT_USER,
+    user
+  };
+};
+
+export const receiveErrors = (errors) => {
+  return {
+    type: RECEIVE_ERRORS,
+    errors
+  };
+};
+
+
+// Thunk action creators
+
+export const login = (user) => dispatch => {
+  return SessionAPIUtil.login(user)
+    .then((user) => dispatch(receiveCurrentUser(user)),
+    (errors) => dispatch(receiveErrors(errors)));
+};
+
+
+export const logout = () => dispatch => {
+  return SessionAPIUtil.logout()
+    .then(
+      () => dispatch(receiveCurrentUser(null)),
+      (errors) => dispatch(receiveErrors(errors)));
+};
+
+
+export const signup = (user) => dispatch => {
+  return SessionAPIUtil.signup(signup)
+    .then(
+      (user) => dispatch(receiveCurrentUser(user)),
+      (errors) => dispatch(receiveErrors(errors)));
+};
