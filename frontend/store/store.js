4bb73d6bc7eb3200bf6c58ca341a0b1df9c62028
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-04-18 15:22:56 -0400

    Complete store.js

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,5 +1,10 @@
+import { createStore, applyMiddleware } from 'redux';
+import RootReducer from '../reducers/root_reducer';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
+const configureStore = (preloadedState = {}) => {
+  createStore(RootReducer, applyMiddleware(thunk, logger));
+};
 
-
-export default configureStore(
-);
+export default configureStore;
