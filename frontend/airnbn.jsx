a177ae52f423b0ef5199d6fc2fa99674c460ac2c
Tomer Ovadia <tomertomertomer@gmail.com>
2017-04-19 11:23:45 -0400

    Bootstrap current user so user remains logged in based on cookie

--- frontend/airnbn.jsx
+++ frontend/airnbn.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {login, logout, signup} from './util/session_api_util';
 import configureStore from './store/store.js';
 import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
-  window.login = login;
-  window.logout = logout;
-  window.signup = signup;
 
-  const store = configureStore();
+  const preloadedState = {session: {currentUser: window.currentUser, errors: []}};
+
+  const store = configureStore(preloadedState);
 
   const root = document.getElementById('root');
   ReactDOM.render(
