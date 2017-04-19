c7b59e3199527da4403742a6f60e89b7d9ecee38
Tomer Ovadia <tomertomertomer@gmail.com>
2017-04-19 15:31:53 -0400

    Created but didn't verify modal_actions, modal_reducer and added modalReducer to the rootReducer

---
+++ frontend/actions/modal_actions.jsx
@@ -0,0 +1,17 @@
+export const RECEIVE_MODAL = 'RECEIVE_MODAL';
+export const CLEAR_MODAL = 'CLEAR_MODAL';
+
+export const receiveModal = (modalName) => {
+  return {
+    type: RECEIVE_MODAL,
+    activeModal: modalName
+  }
+};
+
+
+export const clearModal = () => {
+  return {
+    type: CLEAR_MODAL,
+    activeModal: null
+  }
+};
---
+++ frontend/reducers/modal_reducer.jsx
@@ -0,0 +1,19 @@
+import {RECEIVE_MODAL, CLEAR_MODAL} from '../actions/modal_actions';
+import merge from 'lodash/merge';
+
+const _initialState = {
+  activeModal: null
+}
+
+export default (oldState = _initialState, action){
+
+  switch(action.type){
+    case RECEIVE_MODAL:
+      return {activeModal: action.activeModal}
+    case CLEAR_MODAL:
+      return {activeModal: null}
+    default:
+      return oldState;
+  }
+  
+}
