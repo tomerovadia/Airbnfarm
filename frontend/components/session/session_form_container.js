26074dda385bccb821aa1b13b23ae93ba1b956c1
Tomer Ovadia <tomertomertomer@gmail.com>
2017-04-19 15:45:36 -0400

    Complete front-end route reconfiguration for modal, mostly in app.jsx but also FormSessionContainer

--- frontend/components/session/session_form_container.js
+++ frontend/components/session/session_form_container.js
@@ -4,18 +4,19 @@ import SessionForm from './session_form';
 import {signup, login} from '../../actions/session_actions';
 
 const mapStateToProps = (state, ownProps) => {
-  let formType = ownProps.path.slice(1);
+  // let formType = ownProps.path.slice(1);
 
   return {
     currentUser: state.session.currentUser,
     errors: state.session.errors,
-    formType
+    formType: ownProps.formType
   };
 };
 
 
 const mapDispatchToProps = (dispatch, ownProps) => {
-  let formType = ownProps.path.slice(1);
+  // let formType = ownProps.path.slice(1);
+  let formType = ownProps.formType;
   let processForm = formType === "login" ? login : signup;
 
   return {
