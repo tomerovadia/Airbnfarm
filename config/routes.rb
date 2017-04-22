74eaa859d9132ea4a99e7a81c7c7358eaa775855
Tomer Ovadia <tomertomertomer@gmail.com>
2017-04-22 12:59:26 -0400

    Configure routes and create action of availability controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     resources :users, only: [:create, :show]
     resource :session, only: [:create, :destroy]
     resources :spots, only: [:create]
+    resources :availabilities, only: [:create, :destroy]
 
   end
 
