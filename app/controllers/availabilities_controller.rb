1e5bdf32b2216e364d324d0f7dff3c6d7bba27e6
Kevin Dam <kevinddam@gmail.com>
2017-04-22 12:59:26 -0400

    Configure routes and create action of availability controller

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -1,2 +1,21 @@
 class AvailabilitiesController < ApplicationController
+
+  def create
+    @availability = Availability.new(availability_params)
+    if @availability.save
+      render json: @availability
+    else
+      render json: @availability.errors, status: 422
+    end
+  end
+
+  def destroy
+
+  end
+
+  private
+  def availability_params
+    params.require(:availability).permit(:date, :spot_id)
+  end
+
 end
