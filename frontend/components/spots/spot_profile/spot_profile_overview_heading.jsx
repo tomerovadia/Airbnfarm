a4e455611d059e04720830f14b47d9479c4273db
Tomer Ovadia <tomertomertomer@gmail.com>
2017-04-23 01:36:47 -0400

    Complete most of templating for spot show page

---
+++ frontend/components/spots/spot_profile/spot_profile_overview_heading.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default (props) => {
+
+  return(
+
+    <div className='spot-profile-overview-heading'>
+
+      <div className='spot-profile-overview-heading-left'>
+        <h1>{props.currentSpot.title}</h1>
+        <div className='spot-profile-city-and-reviews'>
+          {props.currentSpot.city}
+        </div>
+      </div>
+
+
+      <div className='spot-profile-overview-heading-right'>
+        <img src='https://a0.muscache.com/defaults/user_pic-50x50.png?v=2'></img>
+
+        <div className='spot-profile-host-name'>Pepe Pisos & Villas</div>
+      </div>
+
+    </div>
+  );
+
+};
---
+++ frontend/components/spots/spot_profile/spot_profile_overview_thespace.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+export default (props) => {
+
+  return(
+
+    <div className='spot-profile-overview-thespace'>
+
+      <div className='spot-profile-overview-thespace-row'>
+        <div className='spot-profile-overview-thespace-item'>
+          <span>Bathrooms: </span>
+          <span>8</span>
+        </div>
+
+        <div className='spot-profile-overview-thespace-item'>
+          <span>Bedrooms: </span>
+          <span>4</span>
+        </div>
+      </div>
+
+      <div className='spot-profile-overview-thespace-row'>
+        <div className='spot-profile-overview-thespace-item'>
+          <span>Beds:</span>
+          <span>2</span>
+        </div>
+
+        <div className='spot-profile-overview-thespace-item'>
+          <span>Room Type: </span>
+          <span>Entire home/apt</span>
+        </div>
+      </div>
+
+
+    </div>
+  );
+
+};
