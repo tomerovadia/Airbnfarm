aab9c2f2c536557a195360baff2b4a731fcb73b0
Tomer Ovadia <tomertomertomer@gmail.com>
2017-04-24 16:36:12 -0400

    Add calendar to welcome nav

--- frontend/airnbn.jsx
+++ frontend/airnbn.jsx
@@ -10,8 +10,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const store = configureStore(preloadedState);
 
-  window.store = store;
-
   const root = document.getElementById('root');
   ReactDOM.render(
     <Root store={store}/>,
