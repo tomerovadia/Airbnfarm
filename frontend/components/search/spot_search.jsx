b6489f70c04ba48d8ca2c0a59923c427b165d027
Tomer Ovadia <tomertomertomer@gmail.com>
2017-04-23 22:33:12 -0400

    Add basics for search page, like front-end route, container, page, and a few divs with some styling, and css stylesheet

---
+++ frontend/components/search/spot_search.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class SpotSearch extends React.Component {
+
+  constructor(props){
+    super(props);
+
+  }
+
+  render() {
+
+    return(
+      <div className='spot-search-main-container'>
+
+        <div className='spot-search-left'>
+
+
+        </div>
+
+
+
+
+        <div className='spot-search-right'>
+
+          <div className='spot-search-map-div'></div>
+
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default SpotSearch;
