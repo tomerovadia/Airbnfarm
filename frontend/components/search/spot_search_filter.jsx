fd42f2897dd74038173dab0595620c45578a98f2
Tomer <Tomer@tests-MacBook-Air.local>
2017-04-24 13:38:32 -0400

    Create basic layout for spot search filter dropdowns

---
+++ frontend/components/search/spot_search_filter.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default (props) => {
+
+  return (
+
+    <div className='spot-search-filter'>
+      <div className='spot-search-filter-dropdown'>Room Type</div>
+      <div className='spot-search-filter-dropdown'>Price Range</div>
+    </div>
+
+  );
+
+}
