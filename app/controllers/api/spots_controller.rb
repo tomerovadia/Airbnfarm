b1d84db7b83513a60eb21e9e8fa32afd1a4d050c
Tomer Ovadia <tomertomertomer@gmail.com>
2017-04-25 09:23:06 -0400

    Comment out old seeds, factor into model the Spot filter by city

--- app/controllers/api/spots_controller.rb
+++ app/controllers/api/spots_controller.rb
@@ -2,13 +2,7 @@ class Api::SpotsController < ApplicationController
 
   def index
 
-    city = params[:city]
-    if city == ""
-      @spots = Spot.all
-    else
-      @spots = Spot.where(city: city).includes(:privacy_level, :availabilities)
-    end
-
+    @spots = Spot.all_spots_in(params[:city])
 
     if params[:startDate] && params[:endDate]
       startDate = Date.parse(params[:startDate])
@@ -34,23 +28,6 @@ class Api::SpotsController < ApplicationController
     else
       render template: 'api/spots/minishow'
     end
-
-
-    # receive params that include location, start date and end date
-
-    # go through all spots and return those that:
-      # a) have the same location, and
-      # b) have a row in the Availabilities table for each date from the startdate to the endDate
-
-    # return those spots as an object, with the id as the key, and the spot info as the value
-    # the only spot info needed is title, main photo url, price, and num_beds
-    # make a json builder that formats these spots, partial for _spot and one for show that puts them together into an array with .map! or something
-
-    # render :show
-
-    # render errors otherwise:
-      # if no results matched
-      # if another error occurred
 
   end
 
