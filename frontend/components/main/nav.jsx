056af7b4fce94ccf097db11cb09870fdb2da1ce2
Tomer Ovadia <tomertomertomer@gmail.com>
2017-04-25 23:53:29 -0400

    Attempt to fix bugs with calendar on search bar

--- frontend/components/main/nav.jsx
+++ frontend/components/main/nav.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import NavLinks from './nav_links';
-import {Link, hashHistory} from 'react-router';
+import {Link, hashHistory, withRouter} from 'react-router';
 import WelcomeSearchBar from '../search/welcome_search_bar';
 
 export default (props) => {
@@ -14,6 +14,8 @@ export default (props) => {
   }
 
   let loggedIn = props.currentUser ? true : false
+
+  const WelcomeSearchBarWithRouter = withRouter(WelcomeSearchBar);
 
   return (
     <nav className='main-nav'>
@@ -31,7 +33,7 @@ export default (props) => {
 
 
       <div  className='main-nav-center'>
-        <WelcomeSearchBar />
+        <WelcomeSearchBarWithRouter />
       </div>
 
 
