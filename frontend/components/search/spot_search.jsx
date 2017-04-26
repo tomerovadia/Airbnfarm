5c9d7bd257319c5fdad41980946b519aae5c9bab
Tomer Ovadia <tomertomertomer@gmail.com>
2017-04-26 10:14:45 -0400

    Fix calendar flickering issue, fix bug when using searchbar in nav on search page, where it wouldn't update map, now added componentWillUpdate for spot_search, instead of all in constructor

--- frontend/components/search/spot_search.jsx
+++ frontend/components/search/spot_search.jsx
@@ -2,23 +2,36 @@ import React from 'react';
 import SpotSearchFilter from './spot_search_filter';
 import SpotSearchResultsList from './spot_search_results_list';
 import SpotSearchMap from './spot_search_map';
+import { isEqual } from 'lodash.isequal';
 
 class SpotSearch extends React.Component {
 
   constructor(props){
     super(props);
+    this.criteria = this.getCriteriaFromQueryString();
+    this.props.fetchSearchResults(this.criteria);
+  }
 
-    const city = props.location.query.city || '';
+
+  componentWillUpdate() {
+    if(JSON.stringify(this.criteria) !== JSON.stringify(this.getCriteriaFromQueryString())){
+      this.criteria = this.getCriteriaFromQueryString();
+      this.props.fetchSearchResults(this.criteria);
+    }
+  }
+
+  getCriteriaFromQueryString(){
+    const city = this.props.location.query.city || '';
 
     const criteria = {
       city,
-      startDate: props.location.query.startDate,
-      endDate: props.location.query.endDate,
+      startDate: this.props.location.query.startDate,
+      endDate: this.props.location.query.endDate,
     }
 
-    props.fetchSearchResults(criteria);
-
+    return criteria;
   }
+
 
   render() {
 
