756084e5c37df3dbfd6e29b0945882c8eb9f6607
Tomer Ovadia <tomertomertomer@gmail.com>
2017-05-01 10:37:22 -0400

    Change nav links with dropdowns to divs to avoid warning about nested links, dimmed unclikable links and changed their cursor css property

--- frontend/components/main/nav_links.jsx
+++ frontend/components/main/nav_links.jsx
@@ -12,14 +12,14 @@ export default (props) => {
   if(props.loggedIn){
     links =
     <ul className='main-nav-links'>
-      <Link>No time to host?</Link>
-      <Link onClick={() => props.showDropdown('host')}>Host
+      <Link className='inactive'>No time to host?</Link>
+      <div className='link-with-dropdown' onClick={() => props.showDropdown('host')}>Host
         {hostDropdown}
-      </Link>
+      </div>
       <Link to='/bookings/trips'>Trips</Link>
-      <Link>Messages</Link>
-      <Link>Help</Link>
-      <div id='main-nav-avatar' onClick={() => props.showDropdown('userSettings')}>
+      <Link className='inactive'>Messages</Link>
+      <Link className='inactive'>Help</Link>
+      <div className='link-with-dropdown' id='main-nav-avatar' onClick={() => props.showDropdown('userSettings')}>
         <img src='https://a0.muscache.com/defaults/user_pic-50x50.png?v=2' />
         {userSettingsDropdown}
       </div>
@@ -28,7 +28,9 @@ export default (props) => {
   } else {
     links =
     <ul className='main-nav-links'>
+      <Link className='inactive'>No time to host?</Link>
       <Link to='/become-a-host'>Become a Host</Link>
+      <Link className='inactive'>Help</Link>
       <Link onClick={props.activateModal('signup')}>Sign Up</Link>
       <Link onClick={props.activateModal('login')}>Log In</Link>
     </ul>
