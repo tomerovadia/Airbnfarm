ed0e962c1a383b6bbac4ce44945094e7ae760dc9
Tomer Ovadia <tomertomertomer@gmail.com>
2017-06-15 09:38:37 -0400

    Remove all homepage section nav tabs except farms

--- frontend/components/main/homepage_section_nav.jsx
+++ frontend/components/main/homepage_section_nav.jsx
@@ -5,10 +5,7 @@ export default (props) => {
   return(
 
     <div className='homepage-section-nav'>
-      <div className='homepage-section-nav-tab'><span>FOR YOU</span></div>
       <div className='homepage-section-nav-tab selected-tab'><span>FARMS</span></div>
-      <div className='homepage-section-nav-tab'><span>EXPERIENCES</span></div>
-      <div className='homepage-section-nav-tab'><span>PLACES</span></div>
     </div>
   );
 
